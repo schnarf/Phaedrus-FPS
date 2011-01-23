1fb38367810ff62b82e48bb4a92fde9e3f53fa4f
schnarf <schnarf@gmail.com>
2011-01-23 12:53:33 -0500

    Refactored entities a bit to start using components. Big move to boost::python instead of luabind. PlayerEntity now is subclassed in the VM.

--- Resources/WorldStartup.lua
+++
@@ -1,7 +0,0 @@
-function WorldStartup()
-Print( "World started up!" );
-localplayer= World:GetLocalPlayerEntity();
-localplayer.LookVector= Vector(0.0, 0.0, -1.0);
-localplayer.Velocity= Vector(0.0, 0.0, -0.001);
-return 1;
-end
